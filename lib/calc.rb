6fdbbc0ae55e7a6ceaf412f1f25d126e9e72fee9
Fan Jiang <fanjiang@thoughtworks.com>
2014-10-19 12:22:16 +0800

    Revert "specify tokenize and evaluate methods"
    This reverts commit 631acb6dabc88a86959cc0af785a5b5b5b9eff94.

--- lib/calc.rb
+++ lib/calc.rb
@@ -1,26 +1,5 @@
 module Calc
   def self.eval(string)
-    self.evaluate(self.tokenize(string))
-  end
-
-  def self.tokenize(string)
-    [Token.new(:numeric, 1), Token.new(:operator, :+), Token.new(:numeric, 1)]
-  end
-
-  def self.evaluate(tokens)
     2
-  end
-
-  class Token
-    attr_reader :category, :value
-
-    def initialize(category, value)
-      @category = category
-      @value = value
-    end
-
-    def is?(category)
-      @category == category
-    end
   end
 end
