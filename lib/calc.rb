6150649ae54c329cf500c0495dd61a99a4b30504
Fan Jiang <fanjiang@thoughtworks.com>
2014-10-19 13:39:00 +0800

    able to eval plus two numbers

--- lib/calc.rb
+++ lib/calc.rb
@@ -1,5 +1,7 @@
 module Calc
   def self.eval(string)
-    string.to_i
+    result = 0
+    string.split('+').each { |number| result += number.to_i }
+    result
   end
 end
--- spec/calc_spec.rb
+++ spec/calc_spec.rb
@@ -10,12 +10,12 @@ describe 'calc' do
     assert('multiple digits') { Calc.eval('11') == 11 }
   end
 
-  # it 'should eval two number with one operation' do
-  #   assert('plus') { Calc.eval('1+1') == 2 }
+  it 'should eval two number with one operation' do
+    assert('plus') { Calc.eval('1+1') == 2 }
   #   assert('minus') {Calc.eval('1-1') == 0 }
   #   assert('multiple') {Calc.eval('2*2') == 4 }
   #   assert('divide') {Calc.eval('4/2') == 2 }
-  # end
+  end
   #
   # it 'should eval three number with two operation' do
   #   assert('without priority') { Calc.eval('1-1+1') == 1 }
