4185f60ee5c3d483b537733d067199925a66a66e
Zhengquan Yang <yangzhengquan@gmail.com>
2014-10-25 18:28:09 +0800

    refactoring

--- lib/kata_calc.rb
+++ lib/kata_calc.rb
@@ -1,8 +1,7 @@
 class KataCalc
   def eval(input)
     raise StandardError if input.end_with?("\n")
-    input.gsub!(/\n/,'+')
     return input.to_i if not input.include?('+')
-    input.split('+').map(&:to_i).reduce 0, :+
+    input.split(/[\n\+]/).map(&:to_i).reduce 0, :+
   end
 end
